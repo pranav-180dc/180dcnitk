a32abe41e067af35c49a1bbc720323a1b6234f3b
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2023-03-04 16:15:59 +0530

    made responsive

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -12,24 +12,23 @@ function OurServices() {
   return (
     <div>
     <div className='flex flex-col justify-center items-center mt-32'>
-    <div className='text-lime-600 text-7xl font-bold'>OUR SERVICES</div>
-    <div className='px-28 pt-10 font-extralight text-lg'>
-    <p className='text-justify text-2xl'>We are able to support your organization with any challenge it is facing. In addition to developing projects that address your organization's unique challenges, We have also developed special expertise in these six common areas through our work</p>
-    <p className='text-center text-2xl'>with non-profits globally.</p>
-    <p></p>
+    <div className='text-lime-600 text-4xl sm:text-6xl md:text-7xl font-bold p-8 md:p-12'>OUR SERVICES</div>
+    <div className='invisible sm:visible h-2 w-64 md:w-96 rounded-xl bg-clip-content bg-gradient-to-r from-lime-600 to-transparent'></div>
+    <div className='px-10 md:px-14 lg:px-28 pt-10 font-extralight text-lg'>
+    <p className='text-justify text-2xl'>We are able to support your organization with any challenge it is facing. In addition to developing projects that address your organization's unique challenges, We have also developed special expertise in these six common areas through our work with non-profits globally.</p>
     </div>
     </div>
-    <div className='flex flex-row px-28 text-xl font-semibold text-lime-600'>
-        <div className='flex flex-col justify-center items-center basis-1/3 p-10'>
+    <div className='flex flex-col sm:flex-row px-10 md:px-14 lg:px-28 text-base sm:text-sm lg:text-xl font-semibold text-lime-600'>
+        <div className='flex flex-col justify-center items-center basis-1/3 p-2'>
             <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={MEE} alt="..." className='h-10 w-10'/><p>Market Entry and Expansion</p></div>
-            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={IM} alt="..." className='h-10 w-10'/>Impact Measurement</div>
-            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={SEO} alt="..." className='h-10 w-10'/>Search Engine Optimizaion</div>
+            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={IM} alt="..." className='h-10 w-10'/><p>Impact Measurement</p></div>
+            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={SEO} alt="..." className='h-10 w-10'/><p>Search Engine Optimizaion</p></div>
         </div>
-        <div className='flex justify-center items-center basis-1/3 p-10'><img src={middleSVG} alt="..." className='h-32 w-32 drop-shadow-lg'/></div>
-        <div className='flex flex-col justify-center items-center basis-1/3 p-10'>
-            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={MB} alt="..." className='h-10 w-10'/>Marketing and Branding</div>
-            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={OM} alt="..." className='h-10 w-10'/>Operations Management</div>
-            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={FM} alt="..." className='h-10 w-10'/>Financial Management</div>
+        <div className='invisible sm:visible sm:basis-1/3 flex justify-center items-center'><img src={middleSVG} alt="..." className='h-0 w-0 sm:h-28 sm:w-28 lg:h-32 lg:w-32 drop-shadow-lg'/></div>
+        <div className='flex flex-col justify-center items-center basis-1/3 p-2'>
+            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={MB} alt="..." className='h-10 w-10'/><p>Marketing and Branding</p></div>
+            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={OM} alt="..." className='h-10 w-10'/><p>Operations Management</p></div>
+            <div className='flex flex-row p-3 m-5 items-center justify-center bg-white-opac w-full drop-shadow-lg'><img src={FM} alt="..." className='h-10 w-10'/><p>Financial Management</p></div>
         </div>
     </div>
     </div>
--- src/components/WhoAreWe.jsx
+++ src/components/WhoAreWe.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 const WhoAreWe = () => {
   return (
-    <div className='flex flex-row justify-center items-center mt-14'>
-    <div className='flex flex-col justify-center items-center basis-1/2 subpixel-antialiased text-7xl font-bold text-lime-600'>
+    <div className='flex flex-col lg:flex-row justify-center items-center mt-14'>
+    <div className='flex flex-col justify-center items-center p-8 md:p-12 basis-1/2 subpixel-antialiased text-4xl sm:text-6xl md:text-7xl font-bold text-lime-600'>
       WHO ARE WE?
     </div>
-    <div className='h-64 w-2 rounded-xl bg-clip-content bg-gradient-to-b from-lime-600 to-transparent'></div>
-    <div className='flex flex-col basis-1/2 pr-28'>
-        <p className='text-justify p-8 subpixel-antialiased font-extralight text-xl'>180DC is the world's largest consultancy for non-profits and social enterprises.</p>
-        <p className='text-justify p-8 subpixel-antialiased font-extralight text-xl'>180DC provides high-quality business solutions to social impact organizations, with the help of over 10,000 carefully selected and professionally trained student volunteers from across the globe.</p>
-        <p className='text-justify p-8 subpixel-antialiased font-extralight text-xl'>"Why are we called 180 Degrees? It's because we work to turn good organizations into great organizations, challenges into opportunities, ideas into reality. We're focused on positive transformation. Transforming organizations, and - in turn - transforming lives." - Nat Ware (Founder & CEO)</p>
+    <div className='invisible sm:visible h-2 w-64 md:w-96 lg:w-2 lg:h-64 rounded-xl bg-clip-content sm:bg-gradient-to-r lg:bg-gradient-to-b from-lime-600 to-transparent'></div>
+    <div className='flex flex-col basis-1/2 p-8 md:p-12 lg:p-28'>
+        <p className='text-justify p-4 subpixel-antialiased font-extralight text-xl'>180DC is the world's largest consultancy for non-profits and social enterprises.</p>
+        <p className='text-justify p-4 subpixel-antialiased font-extralight text-xl'>180DC provides high-quality business solutions to social impact organizations, with the help of over 10,000 carefully selected and professionally trained student volunteers from across the globe.</p>
+        <div className='flex flex-row justify-center items-center'>
+        <div className='lg:invisible h-72 w-8 sm:h-36 md:w-4 rounded-xl bg-clip-content bg-lime-600'></div>
+        <p className='text-justify p-4 subpixel-antialiased font-extralight text-xl italic'>
+        "Why are we called 180 Degrees? It's because we work to turn good organizations into great organizations, challenges into opportunities, ideas into reality. We're focused on positive transformation. Transforming organizations, and - in turn - transforming lives." - Nat Ware (Founder & CEO)</p>
+        </div>
     </div>
     </div>
   )
