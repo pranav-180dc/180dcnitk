4fd2d08299324a01d95e9340aa57fa89c98969a2
targithu <githubtar@gmail.com>
2023-03-04 22:00:27 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
-// import React from "react";
-// export default function Footer() {
-//     return (
-//    <div id="footer">
-//          <div className='flex basis-full justify-start text-lime-600 h-400'>
-//             REACH US
-//             </div>
-//    </div>
-//     )
-// }
+import React from "react";
+export default function Footer() {
+    return (
+   <div id="footer">
+    <hr class="h-px my-8 bg-lime-200 border-0 dark:bg-lime-700"/>
+         <div className='flex basis-full justify-start text-lime-600 h-400'>
+            REACH US
+            </div>
+        
+   </div>
+    )
+}
--- src/components/WhoAreWe.jsx
+++ src/components/WhoAreWe.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const WhoAreWe = () => {
   return (
-    <div className='md:flex flex-row justify-center items-center mt-14'>
     <div className='md:flex flex-col justify-center items-center basis-1/2 subpixel-antialiased text-7xl font-bold text-lime-600'>
       WHO ARE WE?
     </div>
@@ -11,7 +10,6 @@ const WhoAreWe = () => {
         <p className='text-justify p-8 subpixel-antialiased font-extralight text-xl'>180DC is the world's largest consultancy for non-profits and social enterprises.</p>
         <p className='text-justify p-8 subpixel-antialiased font-extralight text-xl'>180DC provides high-quality business solutions to social impact organizations, with the help of over 10,000 carefully selected and professionally trained student volunteers from across the globe.</p>
         <p className='text-justify p-8 subpixel-antialiased font-extralight text-xl'>"Why are we called 180 Degrees? It's because we work to turn good organizations into great organizations, challenges into opportunities, ideas into reality. We're focused on positive transformation. Transforming organizations, and - in turn - transforming lives." - Nat Ware (Founder & CEO)</p>
-    </div>
     </div>
   )
 }
