67e7d8df16ad4628d221644d73209c0082a83d6f
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2023-03-08 21:31:01 +0530

    created routes

--- src/data/navlinks.js
+++ src/data/navlinks.js
@@ -1,7 +1,7 @@
 export const navlinks = [
     { name: "Home", link: "/", _target:"_self" },
-    { name: "About", link: "/about", _target:"_self" },
-    { name: "Services", link: "/services", _target:"_self" },
-    { name: "Team", link: "/team", _target:"_self" },
-    { name: "Contact", link: "/contact", _target:"_self" },
+    { name: "About", link: "/about", _target:"_blank" },
+    { name: "Services", link: "/services", _target:"_blank" },
+    { name: "Team", link: "/team", _target:"_blank" },
+    { name: "Contact", link: "/contact", _target:"_blank" },
   ];
