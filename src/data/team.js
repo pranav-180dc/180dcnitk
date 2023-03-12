71d58c198ff28fe2c9d6b918f9ce075d1c14a85e
targithu <githubtar@gmail.com>
2023-03-12 19:15:35 +0530

    added most of the team

--- src/data/team.js
+++ src/data/team.js
@@ -1,9 +1,33 @@
-import Ranjeet from '../assets/Ranjeet.jpg'
-import Rohan from '../assets/Rohan.jpeg'
-import Ankit from '../assets/Ankit.jpg'
-import Rahul from '../assets/Rahul.jpeg'
-
-
+import Aaron from '../assets/Pictures/Aaron.png';
+import Aditya from '../assets/Pictures/Aditya.png';
+import Akshay from '../assets/Pictures/Akshay.jpg';
+import Anaghadileep from '../assets/Pictures/Anaghadileep.jpeg';
+import Ankit from '../assets/Pictures/Ankit.jpg';
+import Aryan from '../assets/Pictures/Aryan.jpeg';
+import Ayiman_Mohanty from '../assets/Pictures/Ayiman_Mohanty.jpg';
+import Devvrat_Ashtaputre from '../assets/Pictures/Devvrat_Ashtaputre.jpeg';
+import Kunaal from '../assets/Pictures/Kunaal.png';
+import Mahesh_Alluri from '../assets/Pictures/Mahesh_Alluri.jpg';
+import Mohan from '../assets/Pictures/Mohan.jpg';
+import Onkar from '../assets/Pictures/Onkar.jpg';
+import Pahal from '../assets/Pictures/Pahal.jpeg';
+import Nitin from '../assets/Pictures/Nitin.jpg';
+import Pranav from '../assets/Pictures/Pranav.jpeg';
+import Prathamesh from '../assets/Pictures/Prathamesh.jpg';
+import Pratheek from '../assets/Pictures/pratheek.jpeg';
+import Pratibha from '../assets/Pictures/Pratibha.jpeg';
+import Ranjeet from '../assets/Pictures/Ranjeet.jpg';
+import Rohan from '../assets/Pictures/Rohan.jpeg';
+import Rahul from '../assets/Pictures/Rahul.jpeg';
+import Rakshith from '../assets/Pictures/Rakshith.jpg';
+import Rakshith_N from '../assets/Pictures/Rakshith_N.jpg';
+import Romit from '../assets/Pictures/Romit.jpg';
+import Shivansh from '../assets/Pictures/Shivansh.jpg';
+import Srishti from '../assets/Pictures/Srishti.jpeg';
+import Sujith from '../assets/Pictures/Sujith.jpg';
+import Tarun from '../assets/Pictures/Tarun.jpg';
+import Vishnu from '../assets/Pictures/Vishnu.jpeg';
+import Yajat from '../assets/Pictures/Yajat.jpg';
 const profiles = [{
     id: 1,
     member: "Ranjeet Sonawane",
@@ -46,7 +70,182 @@ const profiles = [{
         line_1: "Intern at PwC US Advisory working in the Health Transformation division.",
         line_2: "A social impact enthusiast eager to make a difference in the non-profit sector."
     } 
-}
+},
+]
+const advisors =[
+    {
+        id: 1,
+        member: "Vishnu Radhakrishnan",
+        imgUrl: Vishnu,
+    },
+    {
+        id: 2,
+        member: "Aaron Sibhu",
+        imgUrl: Aaron,
+    },
+    {
+        id: 3,
+        member: "Romit Shrivastava",
+        imgUrl: Romit,
+    },
+    {
+        id: 4,
+        member: "Shivansh Joshi",
+        imgUrl:Ankit,
+    },
+]
+const consultants = [{
+    id: 1,
+    member: "Vishnu Radhakrishnan",
+    imgUrl: Vishnu,
+},
+{
+    id: 2,
+    member: "Aaron Sibhu",
+    imgUrl:Aaron,
+},
+{
+    id: 3,
+    member: "Romit Shrivastava",
+    imgUrl:Romit,
+},
+{
+    id: 4,
+    member: "Shivansh Joshi",
+    imgUrl: Shivansh,
+},
+{
+    id: 5,
+    member: "Rohan Sunil",
+    imgUrl: Rohan,
+
+},
+{
+    id: 6,
+    member: "Sujith P",
+    imgUrl: Sujith,
+},
+{
+    id: 7,
+    member: "Prathamesh Dongritot",
+    imgUrl: Prathamesh,
+},
+{
+    id: 8,
+    member: "Pratheek Bhat",
+    imgUrl: Pratheek,
+},
+{
+    id: 9,
+    member: "Yajat Bendale",
+    imgUrl: Yajat,
+ 
+},
+{
+    id: 10,
+    member: "Mohan Nayak",
+    imgUrl: Mohan,
+ 
+},
+{
+    id: 11,
+    member: "Anagha Dileep",
+    imgUrl: Anaghadileep,
+ 
+},
+{
+    id: 12,
+    member: "Criss Mendonca",
+    imgUrl: Mohan,
+ 
+},
+{
+    id: 13,
+    member: "Kunaal Joshi",
+    imgUrl: Kunaal,
+ 
+},
+{
+    id: 14,
+    member: "Akshay Srivastava",
+    imgUrl: Akshay,
+ 
+},
+{
+    id: 15,
+    member: "Adithya Shankar",
+    imgUrl: Aditya,
+ 
+},
+{
+    id: 16,
+    member: "Aryan Raul",
+    imgUrl: Aryan,
+ 
+},
+{
+    id: 17,
+    member: "Nitin C",
+    imgUrl: Nitin,
+ 
+},
+{
+    id: 18,
+    member: "Srishti Madhusudan",
+    imgUrl: Srishti,
+ 
+},
+]
+const Designers= [{
+    id: 1,
+    member: "Pavan M",
+    imgUrl: Ranjeet,
+
+},
+{
+    id: 2,
+    member: "Kaveen S",
+    imgUrl: Ranjeet,
+
+},
+{
+    id: 3,
+    member: "Pratibha B",
+    imgUrl: Pratibha,
+
+},
+{
+    id: 4,
+    member: "Rakshith N",
+    imgUrl: Rakshith_N,
+
+},
+{
+    id: 5,
+    member: "Ayiman Mohanty",
+    imgUrl: Ayiman_Mohanty,
+
+},
 ]
 
-export {profiles};
+const Developers= [{
+    id: 1,
+    member: "Pranav Salunkhe",
+    imgUrl: Pranav,
+
+},
+{
+    id: 2,
+    member: "Tarun S Hegde",
+    imgUrl:Tarun,
+
+},
+{
+    id: 3,
+    member: "Pratibha B",
+    imgUrl: Ranjeet,
+
+},
+
+]
+export {profiles,advisors,consultants,Designers,Developers};
--- src/pages/FullTeam.jsx
+++ src/pages/FullTeam.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {profiles} from '../data/team';
+import {profiles,advisors,consultants,Designers,Developers} from '../data/team';
 import FadeInSection from '../components/cards/FadeInSection'
 import { Card } from "flowbite-react";
 
@@ -12,9 +12,7 @@ export default function Team() {
               TEAM
             </div>
           </FadeInSection>
-          <FadeInSection>
             <div className="invisible sm:visible h-2 w-64 md:w-96 rounded-xl bg-clip-content bg-gradient-to-r from-lime-600 to-transparent"></div>
-          </FadeInSection>
         </div>
         <FadeInSection>
           <div className="flex justify-center font-poppins p-3 m-3 text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-lime-700 to-lime-200">
@@ -22,93 +20,92 @@ export default function Team() {
           </div>
         </FadeInSection>
         <FadeInSection>
-        <div className="hidden md:flex ">
-            {profiles.map((profile, idx) => (
-           
-           <Card 
-             imgAlt=".."
-             imgSrc={profile.imgUrl}
-             className="basis-1/4 w-30 p-2"
-             
-           >
-             <h5 className="text-2xl font-bold tracking-tight bg-grey-800 text-lime-600 dark:text-white">
-             {profile.member}
-             </h5>
-             <p className="font-normal text-gray-700 dark:text-gray-400">
-             {profile.position}
-             </p>
-             <p className="font-normal text-gray-700 dark:text-gray-400">
-             {profile.bio.line_1}
-             </p>
-             <p className="font-normal text-gray-700 dark:text-gray-400">
-             {profile.bio.line_2}
-             </p>
-           </Card>
-        
-       ))}
-        </div>
+          <div className="flex flex-col md:flex-row justify-center item-center sm:basis-full  md:basis-1/4">
         {profiles.map((profile, idx) => (
-         <div className="max-w-sm md:hidden">
-         <Card
-         height="h-96"
-           horizontal={true}
-           imgSrc={profile.imgUrl}
-           className="p-2 px-6"
-         >
-           <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-             {profile.member}
-           </h5>
-           <p className="font-normal text-gray-700 dark:text-gray-400">
-           {profile.position}
-           </p>
-         </Card>
+      <div className="max-w-sm bg-white border h-[400] border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700">
+      
+          <img class="rounded-t-lg" src={profile.imgUrl} alt="" />
+
+      
+      <div class="p-5 text-lime-500 bg-slate-300">
+              <h5 class="mb-2 text-2xl font-bold ">{profile.member}</h5>
+          <p class="mb-3 font-normal ">{profile.position}</p>
+          <p class="mb-3 font-normal ">{profile.bio.line_1}</p>
        </div>
+      </div>
         ))}
-{/* 
-              // <div className="flex flex-col  justify-center items-center p-10">
-              //   <div className="flex justify-center items-center h-40 w-40 rounded-full bg-lime-600 border border-stone-300 object-center object-cover shadow-2xl">
-              //     <img src={profile.imgUrl} alt="..." className="h-40 w-40 rounded-full object-left-top object-cover" />
-              //   </div>
-              //   <div className="flex justify-center items-center text-black opacity-60 font-semibold text-2xl">
-              //     {profile.member}
-              //   </div>
-              //   <div className="flex justify-center items-center text-lime-600 font-medium text-xl">
-              //     {profile.position}
-              //   </div>
-              // </div> */}
+      </div>
         </FadeInSection>
         <div className="flex flex-col justify-center items-center text-center ">
           <FadeInSection>
-            <div className="text-lime-600 p-8 md:p-12 text-4xl sm:text-6xl md:text-7xl font-bold">
+            <div className="text-lime-600 p-8 md:p-12 text-4xl text-6xl font-bold">
               Consultants
             </div>
           </FadeInSection>
-          <FadeInSection>
             <div className="invisible sm:visible h-2 w-64 md:w-96 rounded-xl bg-clip-content bg-gradient-to-r from-lime-600 to-transparent"></div>
-          </FadeInSection>
+          
         </div>
         <FadeInSection>
-          <div className="flex justify-center font-poppins p-3 m-3 text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-lime-700 to-lime-200">
-            Executive Core
-          </div>
-        </FadeInSection>
-        <FadeInSection>
-          <div className="md:flex justify-around ">
-            {profiles.map((profile, idx) => (
-              <div className="flex flex-col  justify-center items-center p-10">
-                <div className="flex justify-center items-center h-40 w-40 rounded-full bg-lime-600 border border-stone-300 object-center object-cover shadow-2xl">
-                  <img src={profile.imgUrl} alt="..." className="h-40 w-40 rounded-full object-left-top object-cover" />
+          <div className=" md:flex flex-wrap justify-around p-3">
+            {consultants.map((consultant, idx) => (
+              <div className=" flex flex-col  justify-center items-center  p-12">
+                <div className="flex justify-center items-center h-40 w-45 rounded-full bg-lime-600 border border-stone-300 object-center object-cover shadow-2xl">
+                  <img src={consultant.imgUrl} alt="..." className="h-40 w-40 rounded-full object-cover" />
                 </div>
-                <div className="flex justify-center items-center text-black opacity-60 font-semibold text-2xl">
-                  {profile.member}
-                </div>
-                <div className="flex justify-center items-center text-lime-600 font-medium text-xl">
-                  {profile.position}
+                <div className="flex justify-center items-center text-center text-black opacity-60 font-semibold text-2xl">
+                  {consultant.member}
                 </div>
               </div>
             ))}
           </div>
         </FadeInSection>
+        <div className="flex flex-col justify-center items-center text-center ">
+          <FadeInSection>
+            <div className="text-lime-600 p-8 md:p-12 text-6xl font-bold">
+              Designers
+            </div>
+          </FadeInSection>
+            <div className="invisible sm:visible h-2 w-64 md:w-96 rounded-xl bg-clip-content bg-gradient-to-r from-lime-600 to-transparent"></div>
+          
+        </div>
+        <FadeInSection>
+          <div className="md:flex flex-wrap justify-around p-3">
+            {Designers.map((Designer, idx) => (
+              <div className="flex flex-col  justify-center items-center p-12">
+                <div className="flex justify-center items-center h-40 w-40 rounded-full bg-lime-600 border border-stone-300 object-center object-cover shadow-2xl">
+                  <img src={Designer.imgUrl} alt="..." className="h-40 w-40 rounded-full object-left-top object-cover" />
+                </div>
+                <div className="flex justify-center items-center text-black opacity-60 font-semibold text-2xl">
+                  {Designer.member}
+                </div>
+              </div>
+            ))}
+          </div>
+        </FadeInSection>
+        <div className="flex flex-col justify-center items-center text-center ">
+          <FadeInSection>
+            <div className="text-lime-600 p-8 md:p-12 text-6xl font-bold">
+              Developers
+            </div>
+          </FadeInSection>
+            <div className="invisible sm:visible h-2 w-64 md:w-96 rounded-xl bg-clip-content bg-gradient-to-r from-lime-600 to-transparent"></div>
+          
+        </div>
+        <FadeInSection>
+          <div className="md:flex flex-wrap justify-around p-3">
+            {Developers.map((Developer, idx) => (
+              <div className="flex flex-col  justify-center items-center p-12">
+                <div className="flex justify-center items-center h-40 w-40 rounded-full bg-lime-600 border border-stone-300 object-center object-cover shadow-2xl">
+                  <img src={Developer.imgUrl} alt="..." className="h-40 w-40 rounded-full object-left-top object-cover" />
+                </div>
+                <div className="flex justify-center items-center text-black opacity-60 font-semibold text-2xl">
+                  {Developer.member}
+                </div>
+              </div>
+            ))}
+          </div>
+        </FadeInSection>
+        
       </div>
 
       
