a7bbd992f2318bcc6d0dc251b351e306dd41c99d
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2023-03-20 23:28:10 +0530

    added transition on hover

--- src/pages/FullTeam.jsx
+++ src/pages/FullTeam.jsx
@@ -23,7 +23,7 @@ export default function Team() {
         <FadeInSection>
           <div className="hidden md:flex  md:flex-row md:justify-center md:item-center  md:text-center ">
         {profiles.map((profile, idx) => (
-      <div className="text-lime-500 border-2 bg-opacity-50 border-lime-500  hover:scale-[1.05]  bg-slate-300 m-5 rounded-lg pb-2 shadow ">
+      <div className="text-lime-500 border-2 bg-opacity-50 border-lime-500  hover:scale-[1.05] transition-all delay-150  bg-slate-300 m-5 rounded-lg pb-2 shadow ">
       
           <img class="rounded-t-lg" src={profile.imgUrl} alt="" />
 
@@ -37,7 +37,7 @@ export default function Team() {
       </div>
       <div className="md:hidden flex flex-wrap justify-around p-3">
             {profiles.map((profile, idx) => (
-              <div className=" flex flex-col  border-2 border-lime-500  hover:scale-[1.05]  justify-center text-4xl items-center  p-12">
+              <div className=" flex flex-col  border-2 border-lime-500  hover:scale-[1.05] transition-all delay-150 justify-center text-4xl items-center  p-12">
                 <div className="flex justify-center border border-lime-600 h-40 w-40 items-center rounded-full  object-center object-cover shadow-2xl">
                   <img src={profile.imgUrl} alt="..." className="h-30 w-30 rounded-full object-contain" />
                 </div>
@@ -57,7 +57,7 @@ export default function Team() {
         <FadeInSection>
           <div className="hidden md:flex  md:flex-row md:justify-center md:item-center  md:text-center ">
         {profiles.map((profile, idx) => (
-      <div className="text-lime-500 bg-slate-200 border-2 border-lime-500  hover:scale-[1.05]  bg-white bg-opacity-25 mx-9 rounded-lg  shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="text-lime-500 bg-slate-200 border-2 border-lime-500  hover:scale-[1.05] transition-all delay-150  bg-opacity-25 mx-9 rounded-lg  shadow dark:bg-gray-800 dark:border-gray-700">
           <img class="rounded-t-lg" src={profile.imgUrl} alt="" />
       <div class="px-3  text-center font-poppins">
           <h5 class="mb-2 text-2xl text-slate-600 font-bold ">{profile.member}</h5>
@@ -67,7 +67,7 @@ export default function Team() {
       </div>
       <div className="md:hidden flex flex-wrap justify-around p-3">
             {profiles.map((profile, idx) => (
-              <div className=" flex flex-col  border-2 border-lime-500  hover:scale-[1.05]  justify-center text-4xl items-center  p-12">
+              <div className=" flex flex-col  border-2 border-lime-500  hover:scale-[1.05] transition-all delay-150  justify-center text-4xl items-center  p-12">
                 <div className="flex justify-center items-center h-40 w-40 rounded-full bg-lime-600 border border-stone-300 object-center object-cover shadow-2xl">
                   <img src={profile.imgUrl} alt="..." className="h-30 w-30 rounded-full object-contain" />
                 </div>
@@ -93,7 +93,7 @@ export default function Team() {
             {consultants.map((consultant, idx) => (
               <div className=" flex flex-col  justify-center items-center  p-12">
                 <div className="flex justify-center items-center h-40 w-45 rounded-full bg-lime-600 object-center object-cover shadow-2xl">
-                  <img src={consultant.imgUrl} alt="..." className="border-2 border-lime-300  hover:scale-[1.05]  h-40 w-40 rounded-full object-cover" />
+                  <img src={consultant.imgUrl} alt="..." className="border-2 border-lime-300  hover:scale-[1.05] transition-all delay-150 h-40 w-40 rounded-full object-cover" />
                 </div>
                 <div className="flex justify-center items-center text-center text-black opacity-60 font-semibold text-2xl">
                   {consultant.member}
@@ -113,8 +113,8 @@ export default function Team() {
           <div className="md:flex flex-wrap justify-around p-3">
             {Designers.map((Designer, idx) => (
               <div className="flex flex-col  justify-center items-center p-12">
-                <div className="flex justify-center items-center h-40 w-40 rounded-full bg-lime-600 border border-stone-300 object-center object-cover shadow-2xl">
-                  <img src={Designer.imgUrl} alt="..." className="border-2 border-lime-300  hover:scale-[1.05]  h-40 w-40 rounded-full object-left-top object-cover" />
+                <div className="flex justify-center items-center h-40 w-40 hover:scale-[1.05] transition-all delay-150 rounded-full bg-lime-600 border border-stone-300 object-center object-cover shadow-2xl">
+                  <img src={Designer.imgUrl} alt="..." className="border-2 border-lime-300  h-40 w-40 rounded-full object-left-top object-cover" />
                 </div>
                 <div className="flex justify-center items-center text-black opacity-60 font-semibold text-2xl">
                   {Designer.member}
@@ -137,7 +137,7 @@ export default function Team() {
             {Developers.map((Developer, idx) => (
               <div className="flex flex-col   justify-center items-center p-12">
                 <div className="flex justify-center items-center h-40 w-40 rounded-full bg-lime-600  object-center object-cover shadow-2xl">
-                  <img src={Developer.imgUrl} alt="..." className="h-40 w-40 border-2 border-lime-300  hover:scale-[1.05] rounded-full object-left-top object-cover" />
+                  <img src={Developer.imgUrl} alt="..." className="h-40 w-40 border-2 border-lime-300  hover:scale-[1.05] transition-all delay-150 rounded-full object-left-top object-cover" />
                 </div>
                 <div className="flex justify-center items-center text-black opacity-60 font-semibold text-2xl">
                   {Developer.member}
