a387f851282adb351d38062ab4fe16c89278698a
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2023-03-21 02:30:48 +0530

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ export default function Footer() {
         <div className="flex flex-col basis-1/2 md:p-3">
             <div><p className="text-xl md:text-4xl lg:text-5xl font-bold text-lime-600">REACH US</p></div>
             <div className="flex flex-row justify-start my-2"><img src={mail} alt="..." className="h-7 w-7 m-1"/><p className="text-xs md:text-lg lg:text-xl font-extralight m-1">nitk@180dc.org</p></div>
-            <div className="flex flex-row"><p className="flex flex-row text-xs md:text-lg lg:text-xl font-normal">We'd<div className="text-lime-600 text-xs md:text-lg lg:text-xl font-normal px-1">love to hear</div>from you!</p>
+            <div className="flex flex-row w-max"><p className="flex flex-row text-xs md:text-lg lg:text-xl font-normal">We'd<span className="text-lime-600 text-xs md:text-lg lg:text-xl font-normal px-1">love to hear</span>from you!</p>
             </div>
             <div>
             <p className="text-xs md:text-lg lg:text-xl font-normal">Please don't hesitate to reach out.</p>
@@ -29,7 +29,7 @@ export default function Footer() {
         </div>
         
         <div className="flex flex-col md:flex-row md:justify-end basis-1/3 md:basis-1/2">
-        <div className="flex flex-row h-max items-center md:px-4">
+        <div className="flex flex-col md:flex-row h-max items-start md:items-center md:px-4">
             <div className="items-center justify-center">
             <p className="text-xs md:text-lg font-extralight lg:text-xl">Follow us for more:</p>
             </div>
