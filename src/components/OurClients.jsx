aceb8afb56a4bf8d0cf1d742f5357004aa98d9d7
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2023-03-29 23:41:00 +0530

    added al clients

--- src/components/OurClients.jsx
+++ src/components/OurClients.jsx
@@ -17,8 +17,8 @@ function OurClients() {
           <p>OUR CLIENTS</p>
         </FadeInSection>
       </div>
-
-      <div id='scroll-clients' className='grid grid-cols-8 gap-x-72 mt-4'>
+    <div className='relative flex'>
+      <div className=' animate-marquee grid grid-cols-9 gap-x-72 mt-4'>
             <ClientCard 
                 name="Nostos Homes"
                 imgPath = {nostosHomes}
@@ -62,27 +62,94 @@ function OurClients() {
             <ClientCard 
                 name="Sevan Canteens"
                 imgPath = {TFI}
-                w1="Conceptualized campus ambassador model"
-                w2="Outreach channel analysis"
-                w3="Rubric for institute approach"
-                w4="Streamlined recruitment process"
+                w1="Devised USP and GTM strategy"
+                w2="Fundraising strategies"
+                w3="Social media positioning"
+                w4="Peer-benchmarking and market analysis"
             />
             <ClientCard 
                 name="Aashraya Foundation"
                 imgPath = {TFI}
-                w1="Conceptualized campus ambassador model"
-                w2="Outreach channel analysis"
-                w3="Rubric for institute approach"
-                w4="Streamlined recruitment process"
+                w1="Outreach based on past data"
+                w2="Impact assessment"
+                w3="Program development"
+                w4="Organisational restructuring"
             />
             <ClientCard 
                 name="WEFT"
+                imgPath = {TFI}
+                w1="Designed social media and email campaigns"
+                w2="CSR fundraising strategy"
+                w3="Corporate - Client partnerships"
+                w4="Streamlined recruitment process"
+            />
+      </div>
+      <div className='absolute animate-marquee2 grid grid-cols-8 gap-x-72 mt-4'>
+            <ClientCard 
+                name="Nostos Homes"
+                imgPath = {nostosHomes}
+                w1="Optimised resource utilisation"
+                w2="Fundraising strategies"
+                w3="Database of potential sources/organisations"
+                w4="Both national and international outreach"
+            />
+            <ClientCard 
+                name="Lal10"
+                imgPath = {lal10}
+                w1="Peer Benchmarking"
+                w2="Seo Optimisation"
+                w3="International customer acquisiton strategies"
+                w4="Supply demand analysis"
+            />
+            <ClientCard 
+                name="Wakao Foods"
+                imgPath = {wakaoFoods}
+                w1="SEO optimization"
+                w2="Email Marketing"
+                w3="Social media marketing on WhatsApp/LinkedIn/Instagram"
+                w4="Brand publicity and sales strategies"
+            />
+            <ClientCard 
+                name="Teach For India"
                 imgPath = {TFI}
                 w1="Conceptualized campus ambassador model"
                 w2="Outreach channel analysis"
                 w3="Rubric for institute approach"
                 w4="Streamlined recruitment process"
             />
+            <ClientCard 
+                name="Inchara Foundation"
+                imgPath = {inchara}
+                w1="Conceptualized campus ambassador model"
+                w2="Outreach channel analysis"
+                w3="Rubric for institute approach"
+                w4="Streamlined recruitment process"
+            />
+            <ClientCard 
+                name="Sevan Canteens"
+                imgPath = {TFI}
+                w1="Devised USP and GTM strategy"
+                w2="Fundraising strategies"
+                w3="Social media positioning"
+                w4="Peer-benchmarking and market analysis"
+            />
+            <ClientCard 
+                name="Aashraya Foundation"
+                imgPath = {TFI}
+                w1="Outreach based on past data"
+                w2="Impact assessment"
+                w3="Program development"
+                w4="Organisational restructuring"
+            />
+            <ClientCard 
+                name="WEFT"
+                imgPath = {TFI}
+                w1="Designed social media and email campaigns"
+                w2="CSR fundraising strategy"
+                w3="Corporate - Client partnerships"
+                w4="Streamlined recruitment process"
+            />
+      </div>
       </div>
       
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,20 @@ module.exports = {
 ],
   theme: {
     extend: {
+      keyframes:{
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
       colors: {
         'white-opac': '#E6E6E6',
         'light-black': '#636363',
